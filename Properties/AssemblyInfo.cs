171c2f72303a0ce1697e17f46ec16dd47ba64aae
DevX <141998343+NotDevX@users.noreply.github.com>
2024-01-04 11:33:41 -0700

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RDresses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Devy Shit Products")]
+[assembly: AssemblyProduct("RDresses")]
+[assembly: AssemblyCopyright("Copyright © DSP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e04eda80-c375-4250-b111-4c99c5b3646d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
